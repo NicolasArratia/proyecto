edc6f526c3047c186aaa0acbbb31572c92570de4
NicolasArratia <nicolas.munoz1502@alumnos.ubiobio.cl>
2022-05-10 16:23:12 -0400

    modificacion de login

--- login/login.php
+++ login/login.php
@@ -2,13 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Formulario Login</title>
+    <title> Login</title>
     <link rel="stylesheet" href="../assets/css/style.css">
   </head>
   <body>
     <section class="form-login">
-      <h5>Formulario Login</h5>
-      <input class="controls" type="text" name="usuario" value="" placeholder="Usuario">
+      <h5>Login</h5>
+      <h2 class="encabezado"> Bienvenido a EDIFRED</h2>
+      <input class="controls" type="text" name="nombre" value="" placeholder="Nombre">
+      <input class="controls" type="numb" name="rut" value="" placeholder="Rut">
       <input class="controls" type="password" name="contrasena" value="" placeholder="Contraseña">
       <input class="buttons" type="submit" name="" value="Ingresar">
       <p><a href="#">¿Olvidastes tu Contraseña?</a></p>
