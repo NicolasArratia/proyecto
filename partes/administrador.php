c45f00e3353afac035b5746a5b0d6984ae353e05
NicolasArratia <nicolas.munoz1502@alumnos.ubiobio.cl>
2022-05-23 16:10:16 -0400

    modificaciones varias muchas

--- partes/administrador.php
+++ partes/administrador.php
@@ -22,8 +22,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-9 col-md-8">
-                                <h1 class="font-weight-bold mb-0">Bienvenido Juan</h1>
-                                <h2 class="font-weight-bold mb-0">Dpto N° 201</h2>
+                                <h1 class="font-weight-bold mb-0">Bienvenido Administrador</h1>
                                 
                                 <p class="lead text-muted">Revisa la última información</p>
                             </div>
@@ -86,135 +85,107 @@
 
                           
                           <div class="col-lg-4 my-3">
+
+
+                        
+
+
+                          <!-- personas no pago-->
                             <div class="card rounded-0">
                                 <div class="card-header bg-light">
-                                    <h6 class="font-weight-bold mb-0">Pago mes Mayo</h6>
+                                    <h6 class="font-weight-bold mb-0">Personas Atrasadas</h6>
                                 </div>
                                 <div class="card-body pt-2">
+
                                 <div class="d-flex border-bottom py-2">
                                         <div class="d-flex mr-3">
-                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
+                                         
                                         </div>
                                         <div class="align-self-center">
-                                        <h6 class="text-muted">luz</h6>
-                                            
-                                        <p class="lead text-muted">16.000</p>
-                                          
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
-                                        </div>
-                                        <div class="align-self-center">
-                                        <h6 class="text-muted">Agua</h6>
-                                            
-                                        <p class="lead text-muted">25.000</p>
-                                          
+                                        <h6 class="d-inline-block mb-0">Mario faundez</h6><span class="badge badge-danger ml-2">No pago</span>
+                                        <h6>Dpto N° 201</h6>
+                                          <small class="d-block text-muted">ver</small>
                                         </div>
                                     </div>
 
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
-                                        </div>
-                                        <div class="align-self-center">
-                                        <h6 class="text-muted">Gas</h6>
-                                            
-                                        <p class="lead text-muted">30.500</p>
-                                          
-                                        </div>
-                                    </div>
-
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
-                                        </div>
-                                        <div class="align-self-center">
-                                        <h6 class="text-muted">Otros</h6>
-                                            
-                                        <p class="lead text-muted">33.000</p>
-                                          
-                                        </div>
-                                    </div>
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                          <h2 class="align-self-center mb-0"><i class="far fa-bell"></i></h2>
-                                        </div>
-                                        <div class="align-self-center">
-                                        <h6 class="text-muted">Total</h6>
-                                        <h3 class="font-weight-bold">104.000</h3>
-                                        <span class="badge badge-success ml-2" >total a pagar</span>
-                                          <br>
-                                        </div>
-                                    </div>
-                                    
-                                    <br>
-                                    <button class="btn btn-primary w-100">Pagar</button>
-                                </div>
-                            </div>
-
-                            <!-- parte de gastos pagados-->
-                          </div>
-                            <div class="col-lg-4 my-3">
-                            <div class="card rounded-0">
-                                <div class="card-header bg-light">
-                                    <h6 class="font-weight-bold mb-0">gastos pagados mensualmente</h6>
-                                </div>
-                                <div class="card-body pt-2">
                                     <div class="d-flex border-bottom py-2">
                                         <div class="d-flex mr-3">
                                          
                                         </div>
                                         <div class="align-self-center">
-                                        <h6 class="d-inline-block mb-0">Enero</h6><span class="badge badge-success ml-2">gastos pagados</span>
-                                          <small class="d-block text-muted">ver</small>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                          
-                                        </div>
-                                        <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">Febrero</h6><span class="badge badge-success ml-2">gastos pagados</span>
+                                        <h6 class="d-inline-block mb-0">Mario faundez</h6><span class="badge badge-danger ml-2">No pago</span>
+                                        <h6>Dpto N° 201</h6>
                                           <small class="d-block text-muted">ver</small>
                                         </div>
                                     </div>
 
+                                    
                                     <div class="d-flex border-bottom py-2">
                                         <div class="d-flex mr-3">
-                                          
+                                         
                                         </div>
                                         <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">Marzo</h6><span class="badge badge-success ml-2">gastos pagados</span>
+                                        <h6 class="d-inline-block mb-0">Mario faundez</h6><span class="badge badge-danger ml-2">No pago</span>
+                                        <h6>Dpto N° 201</h6>
                                           <small class="d-block text-muted">ver</small>
                                         </div>
                                     </div>
-
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                       
-                                        </div>
-                                        <div class="align-self-center">
-                                          <h6 class="d-inline-block mb-0">Abril</h6><span class="badge badge-success ml-2">gastos pagados</span>
-                                          <small class="d-block text-muted">ver</small>
-                                        </div>
-                                    </div>
-
-                                    <div class="d-flex border-bottom py-2">
-                                        <div class="d-flex mr-3">
-                                      
-                                        </div>
-                                        <div class="align-self-center">
-                                        <h6 class="d-inline-block mb-0">Mayo</h6><span class="badge badge-warning ml-2">por pagar</span>
-                                          <small class="d-block text-muted">ver</small>
-                                        </div>
-                                    </div>
-
-                                   
+                               
                                     
                                     
+                                    
+                                    <button class="btn btn-primary w-100">Pagar</button>
+                                </div>
+                            </div>
+
+
+
+                            
+
+                            <!-- parsonas que han pagado-->
+                          </div>
+                            <div class="col-lg-4 my-3">
+                            <div class="card rounded-0">
+                                <div class="card-header bg-light">
+                                    <h6 class="font-weight-bold mb-0">personas al dia</h6>
+                                </div>
+                                <div class="card-body pt-2">
+
+
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                         
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="d-inline-block mb-0">Mario faundez</h6><span class="badge badge-success ml-2">pagados</span>
+                                        <h6>Dpto N° 201</h6>
+                                          <small class="d-block text-muted">ver</small>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                         
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="d-inline-block mb-0">Mario faundez</h6><span class="badge badge-success ml-2">pagados</span>
+                                        <h6>Dpto N° 201</h6>
+                                          <small class="d-block text-muted">ver</small>
+                                        </div>
+                                    </div>
+
+                                    
+                                    <div class="d-flex border-bottom py-2">
+                                        <div class="d-flex mr-3">
+                                         
+                                        </div>
+                                        <div class="align-self-center">
+                                        <h6 class="d-inline-block mb-0">Mario faundez</h6><span class="badge badge-success ml-2">pagados</span>
+                                        <h6>Dpto N° 201</h6>
+                                          <small class="d-block text-muted">ver</small>
+                                        </div>
+                                    </div>
+ 
                                     <button class="btn btn-primary w-100">Ver Mas</button>
                                 </div>
                             </div>
@@ -237,10 +208,13 @@
                                          
                                         </div>
                                     </div>
-
-                                    
-                                    
-                                    <button class="btn btn-primary w-100">Ver Mas</button>
+                                    <!--boton para href de pag-->
+                                    <form>
+                                        <a href="/edifred/proyecto/login/login.php">
+                                        <input type="button" class="btn btn-primary w-100" value="click here">
+                                         </a>
+                                    </form>
+                                     <!--boton para href de pag-->
                                 </div>
                             </div>
                             <br>
@@ -250,7 +224,16 @@
                                   </div>
                                   <div class="card-body">
                                     <canvas id="myChart" width="300" height="150"></canvas>
+                                    <div>
+                                    <form>
+                                        <a href="/edifred/proyecto/partes/graficohistorico.php">
+                                        <input type="button" class="btn btn-primary w-100" value="ver registro">
+                                         </a>
+                                    </form>
+                                    </div>
+                                    
                                   </div>
+                                  
                               </div>
 
                       </div>
--- partes/sidebar.php
+++ partes/sidebar.php
@@ -7,14 +7,14 @@
                 <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
                     Inicio</a>
                     
-                    <a href="../login" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
+                    <a href="/edifred/proyecto/login/login.php" class="d-block text-light p-3 border-0"><i class="fas fa-tachometer-alt"></i>
                     login</a>
 
-                <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-users"></i>
+                <a href="/edifred/proyecto/partes/detallegastos.php" class="d-block text-light p-3 border-0"><i class="fas fa-users"></i>
                     Usuarios</a>
 
-                <a href="https://www.google.cl" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
-                    Arriendos</a>
+                <a href="/edifred/proyecto/partes/administrador.php" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
+                    Administrador</a>
 
                 <a href="../" class="d-block text-light p-3 border-0"><i class="fas fa-drumstick-bite"></i>
                 Gastos Comunes</a>
